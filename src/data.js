0232bc63a48ef00bd5f734717bb3e2fdb4e9944a
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-08-02 08:52:04 -0600

    Organizacion de datos para historia 1

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-// estas funciones son de ejemplo
-
 export const example = () => {
   return 'example';
 };
@@ -7,5 +5,3 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
-
-// Hola lolaaa ahhhhhhh
