f8046ab913d1767d291679f95213c6e0a47b219d
Yeri Marbi <you@example.com>
2022-08-05 19:21:52 -0500

    mostrando los pokemons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import {} from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//const str = '{ "name", "resistant", "weaknesses"}';
 
-console.log(example, data);
+const allPokemons = (data) => {
+  let verTodos = '';
+  data.pokemon.forEach((obj) => {
+    verTodos += `
+        <img src="${obj.img}"/>
+        <p>${obj.name}</p>`;
+  });
+  return verTodos;
+};
+  document.getElementById('allpokemons').innerHTML = allPokemons(data);
+
+
