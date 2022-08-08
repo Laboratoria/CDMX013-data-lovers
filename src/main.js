8aeb4519b037ece8ea8068d8e33607968df0868d
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-08-08 10:09:26 -0600

    Listo el  uso de clone

--- src/main.js
+++ src/main.js
@@ -12,33 +12,28 @@ btnClose.addEventListener("click", function () {
     textNav.style.display = "none";
 });
 
-function divCreator (dataElement){
-    let div = document.createElement('div');
-    div.id = "div" + dataElement.id;
-    div.className = "organizedData";
-    document.getElementById('bigDivs').appendChild(div);
-    let elementName = document.createElement('p');
-    elementName.innerHTML = dataElement.name;
-    elementName.className = "dataName";
-    document.getElementById("div" + dataElement.id).appendChild(elementName);
-    let image = document.createElement('img');
-    image.src = dataElement.img;
-    image.className = "DataImage";
-    image.id = dataElement.id;
-    document.getElementById("div" + dataElement.id).appendChild(image);
-    image.addEventListener('click', function () { dataWindow(image.id); });  
+let firstDiv = document.getElementById("cloneSection");
+
+let cloner = (filmData) => {
+    let secondDiv = firstDiv.cloneNode(true);
+    secondDiv.children[1].src = filmData.img;
+    secondDiv.children[0].innerHTML = filmData.name;
+    secondDiv.id = filmData.id;
+    secondDiv.addEventListener('click', function () { dataWindow(secondDiv.id); });
+    document.getElementById('bigDivs').appendChild(secondDiv);
+    secondDiv.style.display = 'block';
 }
 
 for (let x = 0; x < data.films.length; x++) {
-    divCreator(data.films[x]);
+    cloner(data.films[x]);
     data.films[x].people.forEach(character => {
-        divCreator(character);
+        cloner(character);
     });
     data.films[x].vehicles.forEach(vehicle => {
-        divCreator(vehicle);
+        cloner(vehicle);
     });
     data.films[x].locations.forEach(location => {
-        divCreator(location);
+        cloner(location);
     });
 }
 
@@ -48,7 +43,7 @@ function dataWindow(filmName) {
     showData.style.display = "block"
     for (let x = 0; x < data.films.length; x++) {
         if (filmName === data.films[x].id) {
-            document.getElementById('data1').innerHTML = ('Name: ' + data.films[x].title);
+            document.getElementById('data1').innerHTML = ('Name: ' + data.films[x].name);
             document.getElementById('data2').innerHTML = ('Description: ' + data.films[x].description);
             document.getElementById('data3').innerHTML = ('Director: ' + data.films[x].director);
             document.getElementById('data4').innerHTML = ('Producer: ' + data.films[x].producer);
@@ -64,7 +59,7 @@ function dataWindow(filmName) {
                 document.getElementById('data3').innerHTML = ('Eye color: ' + name.eye_color);
                 document.getElementById('data4').innerHTML = ('Gender: ' + name.gender);
                 document.getElementById('data5').innerHTML = ('Hair color: ' + name.hair_color);
-                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].title);
+                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
                 }
                 });
                 data.films[x].vehicles.forEach(name => {
@@ -74,7 +69,7 @@ function dataWindow(filmName) {
                 document.getElementById('data3').innerHTML = ('Vehicle class: ' + name.vehicle_class);
                 document.getElementById('data4').innerHTML = ('Pilot: ' + name.pilot.name);
                 document.getElementById('data5').innerHTML = ('Length: ' + name.length);
-                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].title);
+                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
                 }
                 });
                 data.films[x].locations.forEach(name => {
@@ -84,7 +79,7 @@ function dataWindow(filmName) {
                 document.getElementById('data3').innerHTML = ('Residents: ' + name.residents);
                 document.getElementById('data4').innerHTML = ('Water surface: ' + name.surface_water);
                 document.getElementById('data5').innerHTML = ('Terrain: ' + name.terrain);
-                document.getElementById('data6').innerHTML = ('Appears in: ' + data.films[x].title);
+                document.getElementById('data6').innerHTML = ('Appears in: ' + data.films[x].name);
                 }
                 });
         }
