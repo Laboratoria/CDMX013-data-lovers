a473b6b71a535ea0f18a1d1a3e3228f346a23df6
MarissaZavala <107640427+MarissaZavala@users.noreply.github.com>
2022-08-22 12:33:39 -0500

    realizar primer testeo

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filterByType = (type, pokemons) => {
+  let arrayfiltrado = pokemons.filter(pokemon => pokemon.type[0] == type.toLowerCase() || pokemon.type[1] == type.toLowerCase());
+  return arrayfiltrado;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const sortByAsc = (a, b) => {
+  if(a.name > b.name) {
+    return 1;
+  }else{
+    return -1;
+  }
+
+}
+
+export const sortByDsc = (a, b) => {
+  if(a.name < b.name) {
+    return 1;
+  }else{
+    return -1;
+  }
+
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,96 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { filterByType, sortByAsc, sortByDsc } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const generadorHTML = (pokemon) => {
+    const container = document.createElement("div")
+    for (let index = 0; index < pokemon.length; index++) {
+        const element = pokemon[index];
+        const card =document.createElement("div")
+        card.classList.add("card");
+
+        const front = document.createElement("div")
+        front.classList.add("pokemonFront")
+        front.classList.add("growFront")
+        
+
+        const pokeName = document.createElement("h2")
+        pokeName.textContent = element.name;
+        pokeName.classList.add("namePokemon");
+
+        const img = document.createElement("img")
+        img.setAttribute("src", element.img)
+        img.classList.add("imagePokemon")
+
+        const pokeType = document.createElement("h4")
+        pokeType.textContent = element.type;
+        pokeType.classList.add("typePokemon")
+
+
+        const back = document.createElement("div")
+        back.classList.add("pokemonBack")
+        
+        back.classList.add("growBack")
+
+        const resistant = document.createElement("h2")
+        resistant.textContent = element.resistant
+        const imgResistant = document.getElementById("imgResistant")
+        
+        
+        resistant.classList.add("resistant");
+    
+        const weaknesses = document.createElement("h2")
+        weaknesses.textContent = element.weaknesses
+        weaknesses.classList.add("weaknesses");
+
+        const height = document.createElement("h2")
+        let height1 = "HEIGHT: "
+        height.textContent = height1 + element.size.height
+        height.classList.add("height");
+
+        const weight = document.createElement("h2")
+        let weight1 ="WEIGHT: "
+        weight.textContent = weight1  +  element.size.weight
+        weight.classList.add("weight");
+
+
+        front.append(img, pokeName, pokeType,)
+        back.append(resistant,imgResistant, weaknesses,height,weight)
+        card.append(front,back)
+        container.append(card)
+    
+    }
+    return container
+}
+const allpokemons = document.getElementById("allpokemons")
+allpokemons.classList = "pokeStyle"
+const pokemons = data.pokemon
+allpokemons.append(generadorHTML(pokemons));
+
+const selecttype = document.getElementById('pokemon-types');
+selecttype.addEventListener('change', () => {
+    let results = "";
+    if (selecttype.value === '') {
+        results = generadorHTML(pokemons);
+
+    } else {
+        let c = filterByType(selecttype.value, pokemons);
+        results = generadorHTML(c);
+    }
+    allpokemons.innerHTML = results.innerHTML;
+});
+
+const selectSort = document.getElementById("sortby");
+selectSort.addEventListener('change', (e) => {
+
+    let results = '';
+    if (e.target.value == 0) {
+        results = generadorHTML(pokemons.sort(sortByAsc));
+
+    } else {
+        results = generadorHTML(pokemons.sort(sortByDsc));
+
+    }
+    allpokemons.innerHTML = results.innerHTML;
+
+});
+
