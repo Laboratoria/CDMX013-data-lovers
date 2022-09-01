1eb37ad62483c61c5194fc2cc96d59e3ff8912cf
MarissaZavala <107640427+MarissaZavala@users.noreply.github.com>
2022-09-01 18:56:41 -0500

    añadir los cálculos

--- src/main.js
+++ src/main.js
@@ -1,17 +1,39 @@
-import { filterByType, sortByAsc, sortByDsc } from './data.js';
+import { filterByType, sortByAsc, sortByDsc, calculateSTAB, calculateDPS, calculateEPS, } from './data.js';
 import data from './data/pokemon/pokemon.js';
+const buttonUp = document.querySelector('.buttonUp');
+
+const scrollFunction = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        buttonUp.classList.add('showScrollUp');
+    } else {
+        buttonUp.classList.remove('showScrollUp');
+    }
+};
+
+window.addEventListener('scroll', scrollFunction);
+
+const backTotop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+};
+buttonUp.addEventListener('click', backTotop);
+
+const pokemons = data.pokemon
 
 const generadorHTML = (pokemon) => {
     const container = document.createElement("div")
+    container.classList.add("allcards")
     for (let index = 0; index < pokemon.length; index++) {
         const element = pokemon[index];
-        const card =document.createElement("div")
+        const card = document.createElement("div")
         card.classList.add("card");
 
         const front = document.createElement("div")
         front.classList.add("pokemonFront")
         front.classList.add("growFront")
-        
+
 
         const pokeName = document.createElement("h2")
         pokeName.textContent = element.name;
@@ -28,42 +50,62 @@ const generadorHTML = (pokemon) => {
 
         const back = document.createElement("div")
         back.classList.add("pokemonBack")
-        
         back.classList.add("growBack")
 
         const resistant = document.createElement("h2")
         resistant.textContent = element.resistant
-        const imgResistant = document.getElementById("imgResistant")
-        
-        
         resistant.classList.add("resistant");
-    
+
         const weaknesses = document.createElement("h2")
         weaknesses.textContent = element.weaknesses
         weaknesses.classList.add("weaknesses");
 
         const height = document.createElement("h2")
-        let height1 = "HEIGHT: "
-        height.textContent = height1 + element.size.height
+        height.textContent = element.size.height
         height.classList.add("height");
 
         const weight = document.createElement("h2")
-        let weight1 ="WEIGHT: "
-        weight.textContent = weight1  +  element.size.weight
+        weight.textContent = element.size.weight
         weight.classList.add("weight");
+        const about = document.createElement("h2")
+        about.textContent = element.about
+        about.classList.add("about");
 
+        const pokemonStats = document.createElement('div');
+        pokemonStats.classList.add('stats');
+        pokemonStats.innerHTML = "<span class=attacksTitle>Special Attacks</span>";
+
+        const pokemonAttackName = document.createElement('table');
+        pokemonAttackName.classList.add('pokemonAttack');
+        pokemonAttackName.innerHTML = "<tr>" + "<th class=headerTable>Name</th>" + "<th class=headerTable>STAB</th>" + "<th class=headerTable>DPS</th>" + "<th class=headerTable>EPS</th>" + "</tr>";
+        let specialattack = "";
+        let specialAttacks = element["special-attack"];
+
+        for (let attack of specialAttacks) {
+            specialattack =
+
+                "<tr>" + "<th><span class=attackNameTable>" + attack.name + "</span></th>" +
+                "<th><span class=attackStats>" + calculateSTAB(attack["base-damage"], attack["type"], element["type"]) + "</span></th>" +
+                "<th><span class=attackStats>" + calculateDPS(attack["base-damage"], attack["type"], element["type"], attack["move-duration-seg"]) + "</span></th>" +
+                "<th><span class=attackStats>" + calculateEPS(attack["energy"], attack["move-duration-seg"]) + "</span></th>" +
+                "</tr>";
+
+            pokemonAttackName.innerHTML = pokemonAttackName.innerHTML + specialattack;
+        }
 
         front.append(img, pokeName, pokeType,)
-        back.append(resistant,imgResistant, weaknesses,height,weight)
-        card.append(front,back)
+        back.append(resistant, weaknesses, height, weight, about, pokemonStats, pokemonAttackName)
+        card.append(front, back)
         container.append(card)
-    
+
     }
     return container
+
 }
+
 const allpokemons = document.getElementById("allpokemons")
 allpokemons.classList = "pokeStyle"
-const pokemons = data.pokemon
+
 allpokemons.append(generadorHTML(pokemons));
 
 const selecttype = document.getElementById('pokemon-types');
@@ -81,7 +123,6 @@ selecttype.addEventListener('change', () => {
 
 const selectSort = document.getElementById("sortby");
 selectSort.addEventListener('change', (e) => {
-
     let results = '';
     if (e.target.value == 0) {
         results = generadorHTML(pokemons.sort(sortByAsc));
@@ -93,4 +134,5 @@ selectSort.addEventListener('change', (e) => {
     allpokemons.innerHTML = results.innerHTML;
 
 });
+
 
