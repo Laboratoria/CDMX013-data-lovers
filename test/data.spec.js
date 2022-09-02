a428fecca83a4b443f12f3eb5df6c3143d4d03f1
yerimarbi <marbizepeda@gmail.com>
2022-09-01 22:51:54 -0500

    Comenzar con el test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,123 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterByType, sortByAsc, sortByDsc, calculateEPS} from '../src/data.js';
+
+const firstTestExpect = [{
+      "num": "001",
+      "name": "bulbasaur",
+      "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ],
+    },
+    {
+      "num": "005",
+      "name": "charmeleon",
+      "pokemon-rarity": "normal",
+      "type": [
+        "fire",
+      ]
+    },
+    {
+      "num": "006",
+      "name": "charizard",
+      "pokemon-rarity": "normal",
+      "type": [
+        "fire",
+        "flying"
+      ]
+    },
+    {
+      "num": "006",
+      "name": "caterpie",
+      "pokemon-rarity": "normal",
+      "type": [
+        "bug",
+      ]
+    }
+];
 
 
-describe('example', () => {
+const secondTestExpect = [{
+  "num": "001",
+  "name": "bulbasaur",
+  "pokemon-rarity": "normal",
+"type": [
+  "grass",
+  "poison"
+],
+},
+{
+  "num": "006",
+  "name": "caterpie",
+  "pokemon-rarity": "normal",
+  "type": [
+    "bug",
+  ]
+},
+{
+  "num": "006",
+  "name": "charizard",
+  "pokemon-rarity": "normal",
+  "type": [
+    "fire",
+    "flying"
+  ]
+},
+{
+  "num": "005",
+  "name": "charmeleon",
+  "pokemon-rarity": "normal",
+  "type": [
+    "fire",
+  ]
+},
+
+];
+
+
+describe('sortByAsc', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof sortByAsc).toBe('function');
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('Debe retornar -1 si los pokemones no están ordenados', () => {
+    expect(firstTestExpect.sort(sortByAsc)).toEqual(firstTestExpect);
+  });
+  it('Debe retornar 1 si los pokemones están ordenados', () => {
+    expect(secondTestExpect.sort(sortByAsc)).toEqual(secondTestExpect);
   });
 });
 
 
-describe('anotherExample', () => {
+describe('sortByDsc', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof sortByDsc).toBe('function');
+  });
+  it('Debe retornar los pokemones desordenados de z-a', () => {
+    expect(firstTestExpect.sort(sortByDsc)).toEqual(firstTestExpect);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('Debe retornar los pokemones ordenados de z-a', () => {
+    expect(firstTestExpect.sort(sortByDsc)).toEqual(firstTestExpect);
+  });
+});
+
+describe('filterByType', () => {
+  it('is a function', () => {
+    expect(typeof filterByType).toBe('function');
+  });
+
+  it('Debe retornar los tipo de pokemon fire', () => {
+    const filterType = filterByType('fire', firstTestExpect)
+    expect(filterType.length).toBe(2);
+  });
+  it('Debe retornar los tipo de pokemon poison', () => {
+    const filterType = filterByType('poison', firstTestExpect)
+    expect(filterType.length).toBe(1);
+  });
+
+});
+describe('calculateEPS', () => {
+  it('is a function', () => {
+    expect(typeof calculateEPS).toBe('function');
   });
 });
